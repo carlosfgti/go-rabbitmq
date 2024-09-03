818e4652fd3c9bf6e23ea2733fe46dc250f88f86
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-09-03 07:27:21 -0300

    feat: connect rabbitmq

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
 func main() {
-	println("Hello, World!")
+	connection, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	defer connection.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ!")
 }
