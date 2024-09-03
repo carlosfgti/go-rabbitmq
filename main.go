d8f2036b28bb1dbc5bf8aff09561c53158caea34
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-09-03 07:32:46 -0300

    feat: declare queue and publish

--- main.go
+++ main.go
@@ -13,5 +13,44 @@ func main() {
 	}
 	defer connection.Close()
 
+	connectChannel(connection)
+
 	fmt.Println("Successfully connected to RabbitMQ!")
+}
+
+func connectChannel(connection *amqp.Connection) {
+	channel, err := connection.Channel()
+	if err != nil {
+		panic(err)
+	}
+	defer channel.Close()
+
+	queue, err := channel.QueueDeclare(
+		"testing", // name
+		false,     // durable
+		false,     // auto delete
+		false,     // exclusive
+		false,     // no wait
+		nil,       // args
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	err = channel.Publish(
+		"",         // exchange
+		queue.Name, // routing key
+		false,      // mandatory
+		false,      // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World!"),
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Queue status:", queue)
+	fmt.Println("Successfully published message")
 }
